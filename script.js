945bb90a7bd4250774012afddbbad40b6548cbb1
NainaBhadoria <nainabhadoria9@gmail.com>
2020-05-14 02:10:13 +0530

    my first js little project

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+window.onload  = function(){
+var resumeForm =  document.getElementById("Resumeform");
+resumeForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("form submitted");
+    var name = event.target.querySelector("#fname").value;
+    var phone = event.target.querySelector("#phone").value;
+    var email= event.target.querySelector("#email").value;
+    var address = event.target.querySelector("#Address").value;
+    var objective = event.target.querySelector("#objective").value;
+    var education = event.target.querySelector("#Education").value;
+    var ex = event.target.querySelector("#JExperience").value;
+    console.log();
 
-alert("Script loaded");
+     });
+
+}
